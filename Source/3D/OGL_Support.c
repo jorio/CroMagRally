289b9dc2bb3b72bcf1a950c21563a92772e5cbe0
Iliyas Jorio <iliyas@jor.io>
2022-03-26 23:53:52 +0100

    Narrower pillarbox shadows

--- Source/3D/OGL_Support.c
+++ Source/3D/OGL_Support.c
@@ -484,13 +484,14 @@ void DrawPillarboxBackground(OGLSetupOutputType* setupInfo, int vpx, int vpy, in
 	glLoadIdentity();
 
 	glDisable(GL_LIGHTING);
-	glDisable(GL_CULL_FACE);
+	glEnable(GL_CULL_FACE);
 	glDisable(GL_DEPTH_TEST);
 	glDisable(GL_BLEND);
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, gPillarboxTexture);
 
-	float dk = .2;
+	float shadowOpacity = 0.85f;
+	float shadowThickness = 0.11f;
 	float z = 0;
 	float textureAR = 1024.0f / 768.0f;
 
@@ -506,6 +507,8 @@ void DrawPillarboxBackground(OGLSetupOutputType* setupInfo, int vpx, int vpy, in
 		float qR1 = 1.0f;
 		float qR2 = -qL1;
 		float qL2 = -1.0f;
+		float qL3 = qR2 - shadowThickness;  // shadow
+		float qR3 = qL1 + shadowThickness;
 
 		float tcT, tcB, tcL1, tcR1, tcL2, tcR2;
 		if (stripeAR <= texRegionAR)
@@ -529,15 +532,32 @@ void DrawPillarboxBackground(OGLSetupOutputType* setupInfo, int vpx, int vpy, in
 			tcR2 = .5f;
 		}
 
+		glColor4f(1,1,1,1);
 		glBegin(GL_QUADS);
-		glColor4f(dk,dk,dk,1);		glTexCoord2f(tcL1, tcB);		glVertex3f(qL1, qB, z);		// Quad 1 (right)
-		glColor4f(1, 1, 1, 1);		glTexCoord2f(tcR1, tcB);		glVertex3f(qR1, qB, z);
-		glColor4f(1, 1, 1, 1);		glTexCoord2f(tcR1, tcT);		glVertex3f(qR1, qT, z);
-		glColor4f(dk,dk,dk,1);		glTexCoord2f(tcL1, tcT);		glVertex3f(qL1, qT, z);
-		glColor4f(1, 1, 1, 1);		glTexCoord2f(tcL2, tcB);		glVertex3f(qL2, qB, z);		// Quad 2 (left)
-		glColor4f(dk,dk,dk,1);		glTexCoord2f(tcR2, tcB);		glVertex3f(qR2, qB, z);
-		glColor4f(dk,dk,dk,1);		glTexCoord2f(tcR2, tcT);		glVertex3f(qR2, qT, z);
-		glColor4f(1, 1, 1, 1);		glTexCoord2f(tcL2, tcT);		glVertex3f(qL2, qT, z);
+		glTexCoord2f(tcL1, tcB);		glVertex3f(qL1, qB, z);		// Quad 1 (right)
+		glTexCoord2f(tcR1, tcB);		glVertex3f(qR1, qB, z);
+		glTexCoord2f(tcR1, tcT);		glVertex3f(qR1, qT, z);
+		glTexCoord2f(tcL1, tcT);		glVertex3f(qL1, qT, z);
+		glTexCoord2f(tcL2, tcB);		glVertex3f(qL2, qB, z);		// Quad 2 (left)
+		glTexCoord2f(tcR2, tcB);		glVertex3f(qR2, qB, z);
+		glTexCoord2f(tcR2, tcT);		glVertex3f(qR2, qT, z);
+		glTexCoord2f(tcL2, tcT);		glVertex3f(qL2, qT, z);
+		glEnd();
+
+		glEnable(GL_BLEND);
+		glDisable(GL_TEXTURE_2D);
+		glBegin(GL_QUADS);
+		glColor4f(0,0,0,shadowOpacity);
+		glVertex3f(qL1, qT, z);		// Quad 1 (right)
+		glVertex3f(qL1, qB, z);
+		glColor4f(0,0,0,0);
+		glVertex3f(qR3, qB, z);
+		glVertex3f(qR3, qT, z);
+		glVertex3f(qL3, qT, z);		// Quad 2 (left)
+		glVertex3f(qL3, qB, z);
+		glColor4f(0,0,0,shadowOpacity);
+		glVertex3f(qR2, qB, z);
+		glVertex3f(qR2, qT, z);
 		glEnd();
 	}
 	else // tallscreen
@@ -552,6 +572,8 @@ void DrawPillarboxBackground(OGLSetupOutputType* setupInfo, int vpx, int vpy, in
 		float qT1 = 1.0;
 		float qB2 = -1.0;
 		float qT2 = -qB1;
+		float qB3 = qT2 - shadowThickness;  // shadow
+		float qT3 = qB1 + shadowThickness;
 
 		float tcL, tcR, tcT1, tcB1, tcT2, tcB2;
 		if (stripeAR <= texRegionAR)
@@ -577,15 +599,32 @@ void DrawPillarboxBackground(OGLSetupOutputType* setupInfo, int vpx, int vpy, in
 			tcB2 = 0.5f + .5f * zoom;
 		}
 
+		glColor4f(1,1,1,1);
 		glBegin(GL_QUADS);
-		glColor4f(dk,dk,dk,1);		glTexCoord2f(tcL, tcB1);		glVertex3f(qL, qB1, z);		// Quad 1 (top)
-		glColor4f(dk,dk,dk,1);		glTexCoord2f(tcR, tcB1);		glVertex3f(qR, qB1, z);
-		glColor4f(1, 1, 1, 1);		glTexCoord2f(tcR, tcT1);		glVertex3f(qR, qT1, z);
-		glColor4f(1, 1, 1, 1);		glTexCoord2f(tcL, tcT1);		glVertex3f(qL, qT1, z);
-		glColor4f(1, 1, 1, 1);		glTexCoord2f(tcL, tcB2);		glVertex3f(qL, qB2, z);		// Quad 2 (bottom)
-		glColor4f(1, 1, 1, 1);		glTexCoord2f(tcR, tcB2);		glVertex3f(qR, qB2, z);
-		glColor4f(dk,dk,dk,1);		glTexCoord2f(tcR, tcT2);		glVertex3f(qR, qT2, z);
-		glColor4f(dk,dk,dk,1);		glTexCoord2f(tcL, tcT2);		glVertex3f(qL, qT2, z);
+		glTexCoord2f(tcL, tcB1);		glVertex3f(qL, qB1, z);		// Quad 1 (top)
+		glTexCoord2f(tcR, tcB1);		glVertex3f(qR, qB1, z);
+		glTexCoord2f(tcR, tcT1);		glVertex3f(qR, qT1, z);
+		glTexCoord2f(tcL, tcT1);		glVertex3f(qL, qT1, z);
+		glTexCoord2f(tcL, tcB2);		glVertex3f(qL, qB2, z);		// Quad 2 (bottom)
+		glTexCoord2f(tcR, tcB2);		glVertex3f(qR, qB2, z);
+		glTexCoord2f(tcR, tcT2);		glVertex3f(qR, qT2, z);
+		glTexCoord2f(tcL, tcT2);		glVertex3f(qL, qT2, z);
+		glEnd();
+
+		glEnable(GL_BLEND);
+		glDisable(GL_TEXTURE_2D);
+		glBegin(GL_QUADS);
+		glColor4f(0,0,0,shadowOpacity);
+		glVertex3f(qL, qB1, z);		// Quad 1 (top)
+		glVertex3f(qR, qB1, z);
+		glColor4f(0,0,0,0);
+		glVertex3f(qR, qT3, z);
+		glVertex3f(qL, qT3, z);
+		glVertex3f(qL, qB3, z);
+		glVertex3f(qR, qB3, z);
+		glColor4f(0,0,0,shadowOpacity);
+		glVertex3f(qR, qT2, z);		// Quad 2 (bottom)
+		glVertex3f(qL, qT2, z);
 		glEnd();
 	}
 
